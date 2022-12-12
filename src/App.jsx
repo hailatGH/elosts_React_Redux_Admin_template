d76faf561fafe08e07a320cfe1d5826a29e8f6be
hailatGH <hailat.alx@gmail.com>
2022-12-12 18:01:11 +0300

    app hot fix

--- src/App.jsx
+++ src/App.jsx
@@ -163,40 +163,42 @@ export default function App() {
             </Popover>
           </div>
           <div className="home">
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/analytics" element={<Analytics />} />
+            <div className="content">
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/analytics" element={<Analytics />} />
 
-              <Route path="/music">
-                <Route index element={<Music />} />
-                <Route path="albums" element={<Albums />} />
-                <Route path="artists" element={<Artists />} />
-                <Route path="geners" element={<Geners />} />
-                <Route path="tracks" element={<Tracks />} />
-              </Route>
+                <Route path="/music">
+                  <Route index element={<Music />} />
+                  <Route path="albums" element={<Albums />} />
+                  <Route path="artists" element={<Artists />} />
+                  <Route path="geners" element={<Geners />} />
+                  <Route path="tracks" element={<Tracks />} />
+                </Route>
 
-              <Route path="/podcast">
-                <Route index element={<Podcast />} />
-                <Route path="episodes" element={<Episodes />} />
-                <Route path="podcastHosts" element={<PodcastHosts />} />
-                <Route path="podcasts" element={<Podcasts />} />
-                <Route path="seasons" element={<Seasons />} />
-              </Route>
+                <Route path="/podcast">
+                  <Route index element={<Podcast />} />
+                  <Route path="episodes" element={<Episodes />} />
+                  <Route path="podcastHosts" element={<PodcastHosts />} />
+                  <Route path="podcasts" element={<Podcasts />} />
+                  <Route path="seasons" element={<Seasons />} />
+                </Route>
 
-              <Route path="/radio">
-                <Route index element={<Radio />} />
-                <Route path="radioHosts" element={<RadioHosts />} />
-                <Route path="stations" element={<Stations />} />
-              </Route>
+                <Route path="/radio">
+                  <Route index element={<Radio />} />
+                  <Route path="radioHosts" element={<RadioHosts />} />
+                  <Route path="stations" element={<Stations />} />
+                </Route>
 
-              <Route path="/playlists" element={<Playlists />} />
-              <Route path="/users" element={<Users />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/help" element={<Help />} />
-              <Route path="/logout" element={<Logout />} />
-              <Route path="/*" element={<Error />} />
-            </Routes>
+                <Route path="/playlists" element={<Playlists />} />
+                <Route path="/users" element={<Users />} />
+                <Route path="/settings" element={<Settings />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/help" element={<Help />} />
+                <Route path="/logout" element={<Logout />} />
+                <Route path="/*" element={<Error />} />
+              </Routes>
+            </div>
           </div>
         </div>
       </section>
